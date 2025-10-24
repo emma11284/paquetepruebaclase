7f4829e9ce4db05796572cd92f6a0a2bef0e522b
emma11284 <schlieper.emma@gmail.com>
2025-10-24 10:20:15 -0300

    agrega documentacion en suma

--- R/suma.R
+++ R/suma.R
@@ -7,6 +7,21 @@ suma <- function(x = 6, y = 9){
 }
 
 
+#' Suma dos números
+#'
+#' suma dos numeros que deben ser positivo.
+#'
+#' @param a un valor o vector numérico.
+#' @param b un valor o vector numérico.
+#'
+#' @returns
+#' un valor o vector numérico con la suma de x más y.
+#'
+#' @examples
+#' suma(10,2)
+#' suma(3,1)
+#'
+#' @export
 suma <- function(a, b) {
 
   if (a < 0 || b < 0) {
@@ -14,3 +29,5 @@ suma <- function(a, b) {
   }
   return(a + b)
 }
+
+
