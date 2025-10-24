3b897afb10b8bd5c94cd25183e5b6e2c5c9bb48b
emma11284 <schlieper.emma@gmail.com>
2025-10-24 10:20:30 -0300

    documentacion datos

---
+++ R/datos.R
@@ -0,0 +1,11 @@
+#' datos aleatorios
+#'
+#' set de datos de ejemplo com datos aleatorios
+#'
+#' @format vector de largo 1000.
+#'
+#' \describe{
+#'  \item{datos}{númerico, de largo 1000.}
+#'}
+"datos"
+
