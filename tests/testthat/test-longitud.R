691e7579b4f031837998bc65340bda2fef3b31f5
emma11284 <schlieper.emma@gmail.com>
2025-10-31 09:58:04 -0300

    agrega test para suma

---
+++ tests/testthat/test-longitud.R
@@ -0,0 +1,6 @@
+test_that("el pasaje de pulgadas a centimetros funciona", {
+
+  expect_equal(object = pulgadas_a_centimetros(1), expected = 2.54)
+
+  })
+
---
+++ tests/testthat/test-suma.R
@@ -0,0 +1,5 @@
+test_that("la suma funciona", {
+
+   expect_equal(object = suma(2,2), expected = 4)
+
+})
